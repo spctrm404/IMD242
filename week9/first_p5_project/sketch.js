94c1fcaf9efe23576c6e2be3ce892e58890d7d22
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2024-11-04 19:04:12 +0900

    end of week 9

--- week9/first_p5_project/sketch.js
+++ week9/first_p5_project/sketch.js
@@ -1,8 +1,29 @@
+let tileNumX = 16;
+let tileNumY = 12;
+
 function setup() {
   createCanvas(640, 480);
 }
 
 function draw() {
-  background(220);
-  circle(mouseX, mouseY, 100);
+  background('#333333');
+  noStroke();
+  fill('cornflowerblue');
+  // for (let column = 0; column < width; column += 40) {
+  //   for (let row = 0; row < height; row += 40) {
+  //     let x = 20 + column;
+  //     let y = 20 + row;
+  //     let diameter = 30;
+  //     circle(x, y, diameter);
+  //   }
+  // }
+  for (let row = 0; row < tileNumY; row++) {
+    for (let column = 0; column < tileNumX; column++) {
+      let tileW = width / tileNumX;
+      let tileH = height / tileNumY;
+      let x = tileW * 0.5 + column * tileW;
+      let y = tileH * 0.5 + row * tileH;
+      ellipse(x, y, tileW, tileH);
+    }
+  }
 }
